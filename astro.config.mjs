a920f3c6443323b8b5ca06ef4c7069769f3d6fe9
Boxter100 <jared.nicolas779@gmail.com>
2025-06-02 00:04:58 -0600

    Despliege

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://Boxter100.github.io",
+  base: "Web-Reintegra",
+  vite: {
+    plugins: [tailwindcss()]
+  }
+});
